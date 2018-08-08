09ce41d46c078c36a505d4a31581207002cad976
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 05:41:31 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,18 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
+  new_array
 end
 
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+new_array=[]
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+hello(["Tim", "Tom", "Jim"]) do |name| 
+  new_array << "Hi, #{name}" 
+  new_array
+end
+array
